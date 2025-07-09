1a8abe05db33ff3acea886a8848671a9178fd814
k.sarathkumar <166106731+sarathkumargit@users.noreply.github.com>
2025-07-09 21:09:39 +0530

    Update vite.config.ts

--- Price-Research/vite.config.ts
+++ Price-Research/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
